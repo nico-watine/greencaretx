71bdba32375fe3e5fef1624da5d48f72be94eaca
Nico Watine <nico@studioncreations.com>
2016-08-27 14:47:03 -0500

    Testing opt/style.css file with index at the moment
    Via gulpfile.js, have successfully ignored the “.mask” styles for the
    mobile overlay
    Move on to services

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var uncss = require('gulp-uncss');
 gulp.task('default', function () {
     return gulp.src('css/style.css')
         .pipe(uncss({
+        	ignore: ['.js-nav-active .mask', '.mask'],
             html: ['http://nicos-mbp.local:5757/', 'http://nicos-mbp.local:5757/services', 'http://nicos-mbp.local:5757/project-galleries', 'http://nicos-mbp.local:5757/our-plants', 'http://nicos-mbp.local:5757/contact']
         }))
         .pipe(gulp.dest('./css/opt'));
