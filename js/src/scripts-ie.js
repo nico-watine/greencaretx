d80ad9a2d94c58582820907f004e0defb7f305eb
Nico Watine <nico@studioncreations.com>
2016-10-10 15:22:21 -0500

    Alter rem scripts-ie note
    Since rem.js is now in bower_components and NOT assets/BC

--- js/src/scripts-ie.js
+++ js/src/scripts-ie.js
@@ -1,6 +1,6 @@
 // :: IMPORTS :: 
 // PREPENDS :: 
-	// /assets/bower_components/REM-unit-polyfill/js/rem.js
+	// /bower_components/REM-unit-polyfill/js/rem.min.js
 
 $('.main-content').css('opacity','0').delay(2000).animate({'opacity':1});
 $('#page-content').css('opacity','0').delay(3000).animate({'opacity':1});
