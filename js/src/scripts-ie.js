66eddf92d2e7e3845a821cffba29de501c49f982
Nico Watine <nico@studioncreations.com>
2016-10-10 14:59:33 -0500

    Updated import note in scripts-is.js
    To full path to rem.js within bower_components

--- js/src/scripts-ie.js
+++ js/src/scripts-ie.js
@@ -1,5 +1,6 @@
 // :: IMPORTS :: 
-// PREPENDS :: REM.js
+// PREPENDS :: 
+	// /assets/bower_components/REM-unit-polyfill/js/rem.js
 
 $('.main-content').css('opacity','0').delay(2000).animate({'opacity':1});
 $('#page-content').css('opacity','0').delay(3000).animate({'opacity':1});
