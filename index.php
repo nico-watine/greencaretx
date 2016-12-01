c636b2ce637f8729ce5dee68b7937c9e650e112d
Nico Watine <nico@studioncreations.com>
2016-12-01 09:59:09 -0600

    Repoint style.css link on index to opt file
    LGTM!

--- index.php
+++ index.php
@@ -14,8 +14,7 @@
 	
 	<meta name="format-detection" content="telephone=no">
 
-	<!-- <link href="<?php autoVer('/css/opt/style.css'); ?>" rel="stylesheet"> -->
-	<link href="<?php autoVer('/css/style.css'); ?>" rel="stylesheet">
+	<link href="<?php autoVer('/css/opt/style.css'); ?>" rel="stylesheet">
 	
 	<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
